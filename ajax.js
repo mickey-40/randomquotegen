18ddbb823a02c1457a1a1294bae967ecc3e0917a
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-27 15:14:17 -0600

    finished ajax.js file to pull from api and shuffling the quotes

--- ajax.js
+++ ajax.js
@@ -1,38 +1,69 @@
-//Load all users
-const btn = document.getElementById("btn");
-btn.addEventListener("click", getUsers);
+const btn = document.querySelector(".get-quotes");
 
-// create function getusers
-function getUsers (e) {
+btn.addEventListener("click", getQuotes);
+
+const number = document.getElementById("number");
+
+
+function getQuotes(e) {
   e.preventDefault();
 
+  if(number.value.length == 0){
+    return alert("Please enter a number")
+  } else {
+    const http = new XMLHttpRequest();
 
-  const http = new XMLHttpRequest();
+    http.open("GET","https://type.fit/api/quotes", true);
 
+    http.onload = function(){
+      if (this.status === 200){
+        // console.log(this.responseText);
 
-  http.open("GET", "users.json", true);
+        const response = shuffle(JSON.parse(this.responseText));
 
-  http.onload = function(){
-    if(this.status === 200){
-      // console.log(this.responseText);
-      const users = JSON.parse(this.responseText);
+        let output = "";
+        // response.forEach(quote => {
+        //   output += `
+        //     <li>Quote: ${quote.text}</li>
+        //     <li>Author: ${quote.author}</li>
+        //     <hr>
+        //   `;
+        // })
+        for(let i = 0; i < response.length; i++){
+          if(i == number.value){
+            break;
+          }
+            output += `
+              <li>Quote: ${response[i].text}</li>
+              <li>Author: ${response[i].author}</li>
+              <hr>
+            `;
+        }
 
-      let output = "";
-      users.forEach(user => {
-        output += `
-          <hr>
-          <ul>
-            <li>ID: ${user.id}</li>
-            <li>Name: ${user.name}</li>
-            <li>Age: ${user.age}</li>
-            <li>Email: ${user.email}</li>
-          </ul>
-        `;
-      });
+        document.querySelector(".quotes").innerHTML = output;
 
-      document.getElementById("users").innerHTML = output;
+      }
     }
-  }
 
   http.send();
+  }
+
+}
+
+// function to shuffle the quotes
+
+function shuffle(quotes){
+  let CI = quotes.length, tempValue, randomIndex;
+
+  // while elements exist in the array
+  while (CI > 0){
+    randomIndex = Math.floor(Math.random() * CI);
+    // decrease CI by 1
+    CI--;
+    // swap the last element with CI
+    tempValue = quotes[CI];
+    quotes[CI] = quotes[randomIndex];
+    quotes[randomIndex] = tempValue;
+  }
+  return quotes;
 }
