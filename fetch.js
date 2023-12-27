1bc3911b0c1a2f67a107428196f903c8c6dab6bf
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-27 15:25:35 -0600

    fetch.js works just like the ajax.js file

--- fetch.js
+++ fetch.js
@@ -1,29 +1,59 @@
-//Load all users
-const btn = document.getElementById("btn");
-btn.addEventListener("click", getUsers);
+const btn = document.querySelector(".get-quotes");
+
+btn.addEventListener("click", getQuotes);
+
+const number = document.getElementById("number");
+
+const url = "https://type.fit/api/quotes";
 
 
-function getUsers(e){
+function getQuotes(e) {
   e.preventDefault();
 
-  fetch("users.json")
-    .then(function(response){
+  if(number.value.length == 0){
+    return alert("Please enter a number")
+  } else {
+    fetch(url)
+    .then(response => {
       return response.json();
     })
-    .then(function(data){
-      // console.log(data);
+    .then(data => {
+      // console.log(JSON.stringify(data));
+      data = shuffle(data);
       let output = "";
-      data.forEach(user => {
-        output += `
-          <hr>
-          <ul>
-            <li>ID: ${user.id}</li>
-            <li>Name: ${user.name}</li>
-            <li>Age: ${user.age}</li>
-            <li>Email: ${user.email}</li>
-          </ul>
-        `;
-      })
-      document.getElementById("users").innerHTML = output;
+
+      for(let i = 0; i < data.length; i++){
+        if(i == number.value){
+          break;
+        }
+          output += `
+            <li>Quote: ${data[i].text}</li>
+            <li>Author: ${data[i].author}</li>
+            <hr>
+          `;
+      }
+
+      document.querySelector(".quotes").innerHTML = output;
+
     })
+  }
+
+}
+
+// function to shuffle the quotes
+
+function shuffle(quotes){
+  let CI = quotes.length, tempValue, randomIndex;
+
+  // while elements exist in the array
+  while (CI > 0){
+    randomIndex = Math.floor(Math.random() * CI);
+    // decrease CI by 1
+    CI--;
+    // swap the last element with CI
+    tempValue = quotes[CI];
+    quotes[CI] = quotes[randomIndex];
+    quotes[randomIndex] = tempValue;
+  }
+  return quotes;
 }
